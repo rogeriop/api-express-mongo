3642493e232fc21085de4181003bdf58fb9b2665
rogeriop <pinheiroxrogerio@gmail.com>
2025-08-01 18:30:49 -0300

    Deixando nossa API resiliente

--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ import routes from "./routes/index.js"
 
 db.on("error", console.log.bind(console, 'Erro de conexão'))
 db.once("open", () => {
-  console.log('conexão com o banco feita com sucesso')
+  console.log('conexão com o banco feita com sucesso');
+  // Verifica o nome do banco conectado
+  console.log("Banco conectado:", db.name || db.db?.databaseName);
 })
 
 const app = express();
