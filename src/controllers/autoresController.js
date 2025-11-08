f3fc41b456586d37fddb1bb2481b20554d635f6f
rogeriop <pinheiroxrogerio@gmail.com>
2025-11-08 16:33:23 -0300

    02. Avançando o tratamento de erros

--- src/controllers/autoresController.js
+++ src/controllers/autoresController.js
@@ -1,20 +1,24 @@
-import mongoose from "mongoose";
+
+import NaoEncontrado from "../erros/NaoEncontrado.js";
+import RequisicaoIncorreta from "../erros/RequisiçãoIncorreta.js";
+import ErroValidacao from "../erros/ErroValidacao.js";
 import autores from "../models/Autor.js";
+import mongoose from "mongoose";
 
 class AutorController {
 
-  static listarAutores = async(req, res) => {
+  static listarAutores = async(req, res, next) => {
     try {
       const autoresResultado = await autores.find();
 
       res.status(200).json(autoresResultado);
       
   } catch (erro) {
-          res.status(500).json({ message: "Erro interno no servidor" });
+       next(erro);
   }
   }
 
-  static listarAutorPorId = async (req, res) => {
+  static listarAutorPorId = async (req, res, next) => {
     
       try {
         const id = req.params.id;
@@ -23,22 +27,16 @@ class AutorController {
         if (autorResultado !== null) {
           res.status(200).send(autorResultado);
         } else {
-          res.status(404).send({message: "Id do Autor não localizado."});
+          next(new NaoEncontrado("Id do Autor não localizado."))
         }
 
       } catch (erro) {
-        if (erro instanceof mongoose.Error.CastError) {
-          res.status(400).send({message: "Um ou mais dados fornecidos estão incorretos."});
-          
-        } else {
-          res.status(500).send({message: "Erro interno de servidor"})
-
-        }
+        next(erro);
       }
     }
   
   
-    static cadastrarAutor = async (req, res) => {
+    static cadastrarAutor = async (req, res, next) => {
       try {
         let autor = new autores(req.body);
   
@@ -46,32 +44,56 @@ class AutorController {
   
         res.status(201).send(autorResultado.toJSON());
       } catch (erro) {
-        res.status(500).send({message: `${erro.message} - falha ao cadastrar Autor.`});
+        next(erro);
       }
     }
   
 
-    static atualizarAutor = async (req, res) => {
+    static atualizarAutor = async (req, res, next) => {
       try {
         const id = req.params.id;
-  
-        await autores.findByIdAndUpdate(id, {$set: req.body});
-  
-        res.status(200).send({message: "Autor atualizado com sucesso"});
+
+        // Valida formato do id antes de consultar o banco
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+          next(new RequisicaoIncorreta("Id do Autor inválido."));
+        }
+
+        // Retorna o documento atualizado ({ new: true }) e executa validações do schema
+        const autorAtualizado = await autores.findByIdAndUpdate(
+          id,
+          { $set: req.body },
+          { new: true, runValidators: true }
+        );
+
+        // Se não encontrou o autor, lança erro 404
+        if (!autorAtualizado) {
+          next(new NaoEncontrado("Id do Autor não localizado."));
+        }
+
+        res.status(200).send({ message: "Autor atualizado com sucesso", autor: autorAtualizado });
       } catch (erro) {
-        res.status(500).send({message: erro.message});
+        // Mapeia erros de validação do mongoose para nossa classe de erro de validação
+        if (erro.name === "ValidationError") {
+          next(new ErroValidacao(erro));
+        }
+
+        next(erro);
       }
     }
   
-    static excluirAutor = async (req, res) => {
+    static excluirAutor = async (req, res, next) => {
       try {
         const id = req.params.id;
   
-        await autores.findByIdAndDelete(id);
-  
-        res.status(200).send({message: "Autor removido com sucesso"});
+        const autorResultado = await autores.findByIdAndDelete(id);
+        
+        if (autorResultado !== null) {
+          res.status(200).send({message: "Autor removido com sucesso"});
+        } else {
+          next(new NaoEncontrado("Id do Autor não localizado."));
+        }
       } catch (erro) {
-        res.status(500).send({message: erro.message});
+        next(erro);
       }
     }
   
